6515db5bbb7cec3850f0923b9af3cd8f303df1fb
TW <113007141+2taewon@users.noreply.github.com>
2025-04-13 00:05:58 +0900

    Update 팰린드롬수.java

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,44 +1,33 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
-
-        // Pseudo 코드 작성하기
-        // 1. bufferedreader로 문자열 입력받기, parseInt로 정수 변경
-        // 2. if (reverse한 값 == a) yes 출력 else no 출력
-        // 3. 마지막 줄에는 0이 주어진다
-
-
-        // 고민
-        // 1. 정수를 reverse 시키는 함수가 있는가
-        // 2. StringBuilder 사용 후 문자열->정수로 변경하는 방법
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
-        while (true) {
-            // reverse
-            String s = br.readLine();
-            StringBuilder sb = new StringBuilder(s);
-            String reversedStr = sb.reverse().toString();
-
-//            // String -> int로 변환
-//            int a = Integer.parseInt(sb.toString());
-//            int reversedA = Integer.parseInt(reversedStr);
-
-            if (s.equals("0")) break;
-
-            // a와 reversedA가 동일하면 팰린드롬수
-            if (s.equals(reversedStr)) {
-                System.out.println("yes");
-
-            } else {
-                System.out.println("no");
-            }
-
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> list = new ArrayList<String>();
+		while(true) {
+			String str = sc.next();
+			if(str.equals("0")) {
+				break;
+			}
+			if(str.length() == 1) {
+				list.add("yes");
+				continue;
+			}
+		for(int i = 0; i<str.length(); i++) {
+			if(str.charAt(i) == str.charAt(str.length() -(i+1))) {
+				if(str.length() - (i+1) < i) {
+					list.add("yes");
+					break;
+				}
+			}
+			else if(str.charAt(i) != str.charAt(str.length() -(i+1))) {
+				list.add("no");
+				break;
+				}
+			}
+		}
+		for(String s : list)
+			System.out.println(s);
+	}
 }
