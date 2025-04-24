9a1fc1c264b5194cad1a11fd0ce5d782964cd29c
TW <113007141+2taewon@users.noreply.github.com>
2025-04-24 16:37:30 +0900

    [Silver IV] Title: 대칭 차집합, Time: 1420 ms, Memory: 326932 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		int sum = 0;
+		for(int i = 0; i<X; i++) {
+			int A = sc.nextInt();
+			map.put(A, 0);
+		}
+		
+		for(int j = 0; j<Y; j++) {
+			int B = sc.nextInt();
+			if(map.containsKey(B)) {
+				map.remove(B);
+				continue;
+			}
+			map.put(B, 0);
+		}
+		System.out.println(map.size());
+	}
+}
