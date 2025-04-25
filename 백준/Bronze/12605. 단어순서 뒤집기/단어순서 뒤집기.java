a2e8133cc7266940bfe78521fcbe7137d99336db
TW <113007141+2taewon@users.noreply.github.com>
2025-04-25 20:54:44 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 112 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.java
@@ -1,36 +1,24 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String str;
-		int N = Integer.parseInt(br.readLine());
-		for(int num = 1; num <= N; num++) {
-			String temp;
-			str = br.readLine();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+
+		for (int x = 0; x < n; x++) {
+			String str = br.readLine();
 			String[] arr = str.split(" ");
-			for(int i = 0; i < arr.length; i++) {
-				if(i >= arr.length - (i+1)) {
-					break;
-				}
-				temp = arr[i];
-				arr[i] = arr[arr.length - (i+1)];
-				arr[arr.length - (i+1)] = temp;
+
+			System.out.print("Case #" + (x + 1) + ": ");
+			
+			for (int y = arr.length - 1; y >= 0; y--) {
+				System.out.print(arr[y] + " ");
 			}
-			bw.write("Case #"+num +": ");
-			for(String word : arr) {
-				bw.write(word+ " ");
-				}
-			bw.write("\n");
-			}
-		bw.flush();
-		bw.close();
+			System.out.println();
 		}
 	}
-
+}
